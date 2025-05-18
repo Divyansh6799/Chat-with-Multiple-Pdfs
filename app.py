2a442c007e8da91d86a8fe758ad9b5e5be5980ec
Divyansh6799 <divyanshtrivedi0607@gmail.com>
2025-05-18 20:37:12 +0530

    commit

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/gemma-3-1b-it", model_kwargs={"temperature":0.3, "max_length":100},task="text-generation")
+    llm = HuggingFaceHub(repo_id="openai-community/gpt2", model_kwargs={"temperature":0.3, "max_length":100},task="text-generation")
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
