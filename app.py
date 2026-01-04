2482dd8ef6fea975e0e0ef023b90d13c73736db3
Divyansh6799 <divyanshtrivedi0607@gmail.com>
2026-01-04 21:38:20 +0530

    Change Task Name

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-    llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0.3, "max_length":100},task="text-generation")
+    llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0.3, "max_length":100,'task_name':'text2text-generation'})
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
